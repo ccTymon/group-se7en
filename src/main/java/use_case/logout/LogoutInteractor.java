0d2e40bda95f6a94512cdacb0f7860827dcc499a
mustafakashif12 <musti.kashif@mail.utoronto.ca>
2025-11-27 18:19:13 -0500

    Fixed poster bug on movie view. Updated the aesthetics of login view and loggedin view. Added logout user story.

---
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -0,0 +1,27 @@
+package use_case.logout;
+
+/**
+ * The Logout Interactor.
+ */
+public class LogoutInteractor implements LogoutInputBoundary {
+    private LogoutUserDataAccessInterface userDataAccessObject;
+    private LogoutOutputBoundary logoutPresenter;
+
+    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
+                            LogoutOutputBoundary logoutOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
+    }
+
+    @Override
+    public void execute() {
+        // * set the current username to null in the DAO
+        // * instantiate the `LogoutOutputData`, which needs to contain the username.
+        // * tell the presenter to prepare a success view.
+        String a = userDataAccessObject.getCurrentUsername();
+        userDataAccessObject.setCurrentUsername(null);
+        LogoutOutputData logOut = new LogoutOutputData(a);
+        logoutPresenter.prepareSuccessView(logOut);
+    }
+}
+
