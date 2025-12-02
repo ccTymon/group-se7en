cee5fdc7e6a0f1c2db6c59193019970409038b7f
ANIme-A <Tymoncui@gmail.com>
2025-12-01 21:51:02 -0500

    experimental

--- src/main/java/interface_adapter/showmovie/MoviePresenter.java
+++ src/main/java/interface_adapter/showmovie/MoviePresenter.java
@@ -1,4 +1,40 @@
 package interface_adapter.showmovie;
 
-public class MoviePresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.loggedin.LoggedInViewModel;
+import use_case.showmovie.MovieOutputBoundary;
+import use_case.showmovie.MovieOutputData;
+
+public class MoviePresenter implements MovieOutputBoundary {
+    private final MovieSearchModel movieSearchModel;
+
+    public MoviePresenter(ViewManagerModel viewManagerModel, MovieSearchModel movieSearchModel) {
+
+        this.movieSearchModel = movieSearchModel;
+    }
+
+    @Override
+    public void prepareSuccessSaveView(MovieOutputData outputData) {
+        // Update LoggedIn State (Watch Later)
+        //LoggedinState state = loggedInViewModel.getState();
+        // state.setNext_watch(outputData.getMovieName());
+
+        //loggedInViewModel.setState(state);
+        //loggedInViewModel.firePropertyChange();
+
+    }
+
+    @Override
+    public void prepareSuccessReviewView(MovieOutputData outputData) {
+        // Update Movie State (Reviews List)
+
+        // MATT AND MUSTAFA TO IMPLEMENT
+
+
+        MovieState state = movieSearchModel.getState();
+        // state.setReviews(outputData.getReviews()); // IMPLEMENT THIS in MovieState
+
+        movieSearchModel.setState(state);
+        movieSearchModel.firePropertyChange();
+    }
 }
