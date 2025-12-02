634ad7c84403492eeaf2aca5568ba7a0f34af919
ANIme-A <Tymoncui@gmail.com>
2025-12-01 21:50:32 -0500

    experimental

--- src/main/java/use_case/showmovie/MovieOutputBoundary.java
+++ src/main/java/use_case/showmovie/MovieOutputBoundary.java
@@ -1,4 +1,6 @@
 package use_case.showmovie;
 
 public interface MovieOutputBoundary {
+    void prepareSuccessSaveView(MovieOutputData outputData);
+    void prepareSuccessReviewView(MovieOutputData outputData);
 }
